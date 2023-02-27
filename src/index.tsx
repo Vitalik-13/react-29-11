4ddc580a9c4cdba4ba4e88a3bb0728aca0f93a99
vitalikkk <vitalikgermak6@gmail.com>
2023-02-27 14:21:19 +0200

    midified crs/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const title = React.createElement(
-    'h1',
-    {
-        id: 'red',
-        className: 'red',
-    },
-    'Hello react'
+// const title = React.createElement(
+//     'h1',
+//     {
+//         id: 'red',
+//         className: 'red',
+//     },
+//     'Hello react'
+// )
+
+const title = <h1 className="red">Test</h1>
+const list = (
+    <ul>
+        <li>list item</li>
+        <li>list item</li>
+        <li>list item</li>
+    </ul>
+)
+const content = (
+    <div>
+        {title}
+        {list}
+    </div>
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(<React.StrictMode>{content}</React.StrictMode>)
