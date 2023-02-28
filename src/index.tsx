99d3dd651c5e39c408f319d926d2b8635a4a8ad8
vitalikkk <vitalikgermak6@gmail.com>
2023-02-28 15:19:16 +0200

    add, add props with type

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Title from './Title'
-const Content = () => {
-    return (
-        <React.Fragment>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Veritatis earum ipsa consequuntur sed placeat, amet, iusto
-                obcaecati dolore consequatur esse tenetur culpa quis optio
-                perspiciatis velit eligendi eaque, non assumenda.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
-                laborum tempora expedita similique earum mollitia, odio unde,
-                quod quaerat consequatur incidunt consequuntur excepturi
-                necessitatibus enim qui dolorem distinctio laboriosam quis!
-            </p>
-        </React.Fragment>
-    )
-}
+import Content from './content'
+
 const App = () => {
     return (
         <>
@@ -26,7 +10,7 @@ const App = () => {
 
             <Title title="Vitaliy" year={2023} />
 
-            <Content />
+            <Content text1="godby America" text2="hello bro" />
         </>
     )
 }
